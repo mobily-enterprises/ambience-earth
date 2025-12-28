c495c9c0c4c8c786b1e93c5948fbd525ab9c64c1
Tony Mobily <tonymobily@gmail.com>
2025-12-28 14:26:21 +0800

    Move messages to single TU

--- main/messages.h
+++ main/messages.h
@@ -1,174 +1,168 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
-const char PROGMEM MSG_CAL_MOISTURE_SENSOR[] = "Cal moisture sensor";
-const char PROGMEM MSG_MAINTENANCE[] = "Maintenance";
-const char PROGMEM MSG_YES[] = "Yes";
-const char PROGMEM MSG_NO[] = "No";
-const char PROGMEM MSG_OK[] = "OK";
-const char PROGMEM MSG_COLUMN_SPACE[] = ": ";
+extern const char PROGMEM MSG_CAL_MOISTURE_SENSOR[];
+extern const char PROGMEM MSG_MAINTENANCE[];
+extern const char PROGMEM MSG_YES[];
+extern const char PROGMEM MSG_NO[];
+extern const char PROGMEM MSG_OK[];
+extern const char PROGMEM MSG_COLUMN_SPACE[];
 
+extern const char PROGMEM MSG_LITTLE[];
+extern const char PROGMEM MSG_SPACES[];
 
-const char PROGMEM MSG_LITTLE[] = "";
-const char PROGMEM MSG_SPACES[] = "                    ";
+extern const char PROGMEM MSG_SPACE[];
+extern const char PROGMEM MSG_STAR[];
 
-const char PROGMEM MSG_SPACE[] = " ";
-const char PROGMEM MSG_STAR[] = "*";
+extern const char PROGMEM MSG_TRAY_DRY_SHORT[];
+extern const char PROGMEM MSG_TRAY_LITTLE_SHORT[];
+extern const char PROGMEM MSG_TRAY_UNSTATED_SHORT[];
 
-const char PROGMEM MSG_TRAY_DRY_SHORT[] = "dry";
-const char PROGMEM MSG_TRAY_LITTLE_SHORT[] = "wtr";
-const char PROGMEM MSG_TRAY_UNSTATED_SHORT[] = "?";
+extern const char PROGMEM MSG_TRAY_DRY[];
+extern const char PROGMEM MSG_TRAY_LITTLE[];
+extern const char PROGMEM MSG_TRAY_UNSTATED[];
 
-const char PROGMEM MSG_TRAY_DRY[] = "dry";
-const char PROGMEM MSG_TRAY_LITTLE[] = "water in tray";
-const char PROGMEM MSG_TRAY_UNSTATED[] = "unstated";
+extern const char PROGMEM MSG_SOIL_WHEN_DRY[];
+extern const char PROGMEM MSG_SOIL_WHEN_LITTLE_MOIST[];
+extern const char PROGMEM MSG_SOIL_WHEN_MOIST[];
+extern const char PROGMEM MSG_SOIL_WHEN_VERY_MOIST[];
 
+extern const char PROGMEM MSG_SAVE_QUESTION[];
+extern const char PROGMEM MSG_SURE_QUESTION[];
+extern const char PROGMEM MSG_DONE[];
 
-const char PROGMEM MSG_SOIL_WHEN_DRY[] = "Dry means";
-const char PROGMEM MSG_SOIL_WHEN_LITTLE_MOIST[] = "Damp means";
-const char PROGMEM MSG_SOIL_WHEN_MOIST[] = "Mid wet means";
-const char PROGMEM MSG_SOIL_WHEN_VERY_MOIST[] = "Wet means";
+extern const char PROGMEM MSG_MOISTURE_SENSOR[];
 
-const char PROGMEM MSG_SAVE_QUESTION[] = "Save?";
-const char PROGMEM MSG_SURE_QUESTION[] = "Sure?";
-const char PROGMEM MSG_DONE[] = "Done!";
+extern const char PROGMEM MSG_ABORTED[];
 
+extern const char PROGMEM MSG_SOIL[];
+extern const char PROGMEM MSG_SOIL_SHORT[];
 
-const char PROGMEM MSG_MOISTURE_SENSOR[] = "Moisture sensor";
+extern const char PROGMEM MSG_SOIL_NOW[];
+extern const char PROGMEM MSG_TRAY[];
+extern const char PROGMEM MSG_TRAY_SHORT[];
+extern const char PROGMEM MSG_TRAY_NOW[];
+extern const char PROGMEM MSG_PERCENT[];
+extern const char PROGMEM MSG_IS[];
 
-const char PROGMEM MSG_ABORTED[] = "Aborted!";
+extern const char PROGMEM MSG_EXTRACTING[];
+extern const char PROGMEM MSG_FEEDING[];
+extern const char PROGMEM MSG_FEED_LINE[];
 
-const char PROGMEM MSG_SOIL[] = "Soil";
-const char PROGMEM MSG_SOIL_SHORT[] = "Soil";
+extern const char PROGMEM MSG_PUMP_IN[];
 
-const char PROGMEM MSG_SOIL_NOW[] = "Soil now:";
-const char PROGMEM MSG_TRAY[] = "Tray";
-const char PROGMEM MSG_TRAY_SHORT[] = "Tr";
-const char PROGMEM MSG_TRAY_NOW[] = "Tray:";
-const char PROGMEM MSG_PERCENT[] = "%";
-const char PROGMEM MSG_IS[] = "is";
+extern const char PROGMEM MSG_SENSOR_2_MM_WATER[];
 
-const char PROGMEM MSG_EXTRACTING[] = "Extracting...";
-const char PROGMEM MSG_FEEDING[] = "Feeding...";
-const char PROGMEM MSG_FEED_LINE[] = "Feed line";
+extern const char PROGMEM MSG_LINE_IN[];
 
-const char PROGMEM MSG_PUMP_IN[] = "Pump IN";
+extern const char PROGMEM MSG_RESET_DATA[];
+extern const char PROGMEM MSG_RESET[];
+extern const char PROGMEM MSG_TEST_PUMPS[];
+extern const char PROGMEM MSG_LOGS[];
+extern const char PROGMEM MSG_SETTINGS[];
 
-const char PROGMEM MSG_SENSOR_2_MM_WATER[] = "Sensor in 2mm water";
+extern const char PROGMEM MSG_TINY_BIT_OF_WATER[];
+extern const char PROGMEM MSG_HOLD_IT_STILL[];
 
-const char PROGMEM MSG_LINE_IN[] = "Line in...";
+extern const char PROGMEM MSG_YES_HALF_WAY[];
+extern const char PROGMEM MSG_NOW_HALF_WAY[];
+extern const char PROGMEM MSG_YES_FULL_IN[];
+extern const char PROGMEM MSG_NOW_FULL_IN[];
+extern const char PROGMEM MSG_DO_NOT_SUBMERGE[];
 
-const char PROGMEM MSG_RESET_DATA[] = "Reset data";
-const char PROGMEM MSG_RESET[] = "Reset";
-const char PROGMEM MSG_TEST_PUMPS[] = "Test pump";
-const char PROGMEM MSG_LOGS[] = "Logs";
-const char PROGMEM MSG_SETTINGS[] = "Settings";
+extern const char PROGMEM MSG_SENSOR_ATTACHED[];
 
-const char PROGMEM MSG_TINY_BIT_OF_WATER[] = "TINY bit of water";
-const char PROGMEM MSG_HOLD_IT_STILL[] = "Hold it still";
+extern const char PROGMEM MSG_MOIST_SENSOR[];
+extern const char PROGMEM MSG_YES_ITS_DRY[];
+extern const char PROGMEM MSG_ENSURE_SENSOR_IS[];
+extern const char PROGMEM MSG_VERY_DRY[];
+extern const char PROGMEM MSG_VERY_SOAKED[];
+extern const char PROGMEM MSG_YES_ITS_SOAKED[];
 
-const char PROGMEM MSG_YES_HALF_WAY[] = "Yes, it's half in!";
-const char PROGMEM MSG_NOW_HALF_WAY[] = "Now half in water";
-const char PROGMEM MSG_YES_FULL_IN[] = "Yes, it's fully in!";
-const char PROGMEM MSG_NOW_FULL_IN[] = "Now fully in";
-const char PROGMEM MSG_DO_NOT_SUBMERGE[] = "DO NOT SUBMERGE!";
+extern const char PROGMEM MSG_SOAKED_MOIST_SENSOR[];
 
+extern const char PROGMEM MSG_ATTACH_WHITE_SENSOR[];
+extern const char PROGMEM MSG_WHERE_LED_TURNS_ON[];
 
-const char PROGMEM MSG_SENSOR_ATTACHED[] = "Sensor attached?";
+extern const char PROGMEM MSG_OVER[];
 
-const char PROGMEM MSG_MOIST_SENSOR[] = "Moist sensor";
-const char PROGMEM MSG_YES_ITS_DRY[] = "Yes, it's dry!";
-const char PROGMEM MSG_ENSURE_SENSOR_IS[] = "Ensure sensor is";
-const char PROGMEM MSG_VERY_DRY[] = "very DRY";
-const char PROGMEM MSG_VERY_SOAKED[] = "soaked in water";
-const char PROGMEM MSG_YES_ITS_SOAKED[] = "Yes, it's soaked!";
+extern const char PROGMEM MSG_25_PERCENT_COLUMN[];
+extern const char PROGMEM MSG_50_PERCENT_COLUMN[];
+extern const char PROGMEM MSG_75_PERCENT_COLUMN[];
 
+extern const char PROGMEM MSG_DRY_COLUMN[];
+extern const char PROGMEM MSG_SOAKED_COLUMN[];
 
-const char PROGMEM MSG_SOAKED_MOIST_SENSOR[] = "SOAKED moist sensor";
+extern const char PROGMEM MSG_SENSOR_NOT_SENSING_WATER_1[];
+extern const char PROGMEM MSG_SENSOR_NOT_SENSING_WATER_2[];
 
-const char PROGMEM MSG_ATTACH_WHITE_SENSOR[] = "Attach white sensor";
-const char PROGMEM MSG_WHERE_LED_TURNS_ON[] = "where LED turns on";
+extern const char PROGMEM MSG_ERROR_1[];
 
-const char PROGMEM MSG_OVER[] = "Over ";
+extern const char PROGMEM MSG_FULL[];
 
-const char PROGMEM MSG_25_PERCENT_COLUMN[] = "25%: ";
-const char PROGMEM MSG_50_PERCENT_COLUMN[] = "50%: ";
-const char PROGMEM MSG_75_PERCENT_COLUMN[] = "75%: ";
+extern const char PROGMEM MSG_DASH[];
+extern const char PROGMEM MSG_ZERO_PERCENT_DASH[];
+extern const char PROGMEM MSG_PERCENT_DRY[];
+extern const char PROGMEM MSG_PERCENT_DASH[];
+extern const char PROGMEM MSG_PERCENT_SPACE[];
 
-const char PROGMEM MSG_DRY_COLUMN[] = "Dry: ";
-const char PROGMEM MSG_SOAKED_COLUMN[] = "Soaked: ";
+extern const char PROGMEM MSG_PERCENT_DASH_ONEHUNDRED_PERCENT_SPACE[];
 
-const char PROGMEM MSG_SENSOR_NOT_SENSING_WATER_1[] = "Sensor not sensing";
-const char PROGMEM MSG_SENSOR_NOT_SENSING_WATER_2[] = "water";
+extern const char PROGMEM MSG_NO_LOG_ENTRIES[];
+extern const char PROGMEM MSG_RESET_ONLY_LOGS[];
+extern const char PROGMEM MSG_WIPING[];
+extern const char PROGMEM MSG_PRESS_OK_TO_STOP[];
 
-const char PROGMEM MSG_ERROR_1[] = "Error 1";
+extern const char PROGMEM MSG_LOG_TYPE_0[];
+extern const char PROGMEM MSG_LOG_TYPE_1[];
+extern const char PROGMEM MSG_LOG_TYPE_2[];
 
-const char PROGMEM MSG_FULL[] = "full";
+extern const char PROGMEM MSG_LOG_OUTCOME_0[];
+extern const char PROGMEM MSG_LOG_OUTCOME_1[];
 
-const char PROGMEM MSG_DASH[] = "-";
-const char PROGMEM MSG_ZERO_PERCENT_DASH[] = "0\%-";
-const char PROGMEM MSG_PERCENT_DRY[] = "% dry";
-const char PROGMEM MSG_PERCENT_DASH[] = "%-";
-const char PROGMEM MSG_PERCENT_SPACE[] = "% ";
+extern const char PROGMEM MSG_NEXT_FEED[];
+extern const char PROGMEM MSG_LAST_FEED[];
+extern const char PROGMEM MSG_AT_THE_EARLIEST[];
+extern const char PROGMEM MSG_AVG_COLUMN[];
+extern const char PROGMEM MSG_WHEN_CONDITIONS_ALLOW[];
 
-const char PROGMEM MSG_PERCENT_DASH_ONEHUNDRED_PERCENT_SPACE[] = "%-100%"; 
+extern const char PROGMEM MSG_AGO[];
+extern const char PROGMEM MSG_LATER[];
 
-const char PROGMEM MSG_NO_LOG_ENTRIES[] = "No log entries!"; 
-const char PROGMEM MSG_RESET_ONLY_LOGS[] = "Reset only logs"; 
-const char PROGMEM MSG_WIPING[] = "Wiping..."; 
-const char PROGMEM MSG_PRESS_OK_TO_STOP[] = "Press OK to stop..."; 
+extern const char PROGMEM MSG_S_COLUMN[];
+extern const char PROGMEM MSG_W_COLUMN[];
 
-const char PROGMEM MSG_LOG_TYPE_0[] = "BootUp";
-const char PROGMEM MSG_LOG_TYPE_1[] = "Feed";
-const char PROGMEM MSG_LOG_TYPE_2[] = "Values";
+extern const char PROGMEM MSG_NOT_YET[];
+extern const char PROGMEM MSG_NA[];
 
-const char PROGMEM MSG_LOG_OUTCOME_0[] = "Success";
-const char PROGMEM MSG_LOG_OUTCOME_1[] = "Interrupted";
+extern const char PROGMEM MSG_DEVICE_WILL_BLINK[];
+extern const char PROGMEM MSG_THREE_TIMES[];
 
-const char PROGMEM MSG_NEXT_FEED[] = "Next feed: ";
-const char PROGMEM MSG_LAST_FEED[] = "Last feed: ";
-const char PROGMEM MSG_AT_THE_EARLIEST[] = "at the earliest";
-const char PROGMEM MSG_AVG_COLUMN[] = "Avg bw feeds: ";
-const char PROGMEM MSG_WHEN_CONDITIONS_ALLOW[] = "w/conditions allow";
+extern const char PROGMEM MSG_30_PERCENT[];
+extern const char PROGMEM MSG_35_PERCENT[];
+extern const char PROGMEM MSG_40_PERCENT[];
+extern const char PROGMEM MSG_45_PERCENT[];
+extern const char PROGMEM MSG_50_PERCENT[];
+extern const char PROGMEM MSG_55_PERCENT[];
+extern const char PROGMEM MSG_60_PERCENT[];
+extern const char PROGMEM MSG_65_PERCENT[];
+extern const char PROGMEM MSG_70_PERCENT[];
+extern const char PROGMEM MSG_75_PERCENT[];
+extern const char PROGMEM MSG_80_PERCENT[];
+extern const char PROGMEM MSG_85_PERCENT[];
+extern const char PROGMEM MSG_90_PERCENT[];
+extern const char PROGMEM MSG_95_PERCENT[];
+extern const char PROGMEM MSG_100_PERCENT[];
 
+extern const char PROGMEM MSG_TEST_SENSORS[];
 
-const char PROGMEM MSG_AGO[] = "ago";
-const char PROGMEM MSG_LATER[] = "ltr";
+extern const char PROGMEM MSG_TRAY_LOW_COLUMN[];
+extern const char PROGMEM MSG_SOIL_MOISTURE_COLUMN[];
 
-const char PROGMEM MSG_S_COLUMN[] = "S:";
-const char PROGMEM MSG_W_COLUMN[] = "W:";
-
-const char PROGMEM MSG_NOT_YET[] = "Not yet";
-const char PROGMEM MSG_NA[] = "n/a";
-
-const char PROGMEM MSG_DEVICE_WILL_BLINK[] = "Device will blink";
-const char PROGMEM MSG_THREE_TIMES[] = "three times...";
-
-const char PROGMEM MSG_30_PERCENT[] = "30%";
-const char PROGMEM MSG_35_PERCENT[] = "35%";
-const char PROGMEM MSG_40_PERCENT[] = "40%";
-const char PROGMEM MSG_45_PERCENT[] = "45%";
-const char PROGMEM MSG_50_PERCENT[] = "50%";
-const char PROGMEM MSG_55_PERCENT[] = "55%";
-const char PROGMEM MSG_60_PERCENT[] = "60%";
-const char PROGMEM MSG_65_PERCENT[] = "65%";
-const char PROGMEM MSG_70_PERCENT[] = "70%";
-const char PROGMEM MSG_75_PERCENT[] = "75%";
-const char PROGMEM MSG_80_PERCENT[] = "80%";
-const char PROGMEM MSG_85_PERCENT[] = "85%";
-const char PROGMEM MSG_90_PERCENT[] = "90%";
-const char PROGMEM MSG_95_PERCENT[] = "95%";
-const char PROGMEM MSG_100_PERCENT[] = "100%";
-
-const char PROGMEM MSG_TEST_SENSORS[] = "Test sensors";
-
-const char PROGMEM MSG_TRAY_LOW_COLUMN[] =  "Water in tray:";
-const char PROGMEM MSG_SOIL_MOISTURE_COLUMN[] =  "Soil mst:";
-
-const char PROGMEM MSG_PRESS_UP[] = "Press UP";
-const char PROGMEM MSG_PRESS_DOWN[] = "Press DOWN";
-const char PROGMEM MSG_PRESS_LEFT[] = "Press LEFT";
-const char PROGMEM MSG_PRESS_RIGHT[] = "Press RIGHT";
-const char PROGMEM MSG_PRESS_OK[] = "Press OK   ";
+extern const char PROGMEM MSG_PRESS_UP[];
+extern const char PROGMEM MSG_PRESS_DOWN[];
+extern const char PROGMEM MSG_PRESS_LEFT[];
+extern const char PROGMEM MSG_PRESS_RIGHT[];
+extern const char PROGMEM MSG_PRESS_OK[];
 
 #endif MESSAGES_H
