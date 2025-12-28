9dae7710159496d9adb147c913753b563211274b
Tony Mobily <tonymobily@gmail.com>
2025-12-28 14:22:50 +0800

    Document RAM label lifetime

--- main/ui.h
+++ main/ui.h
@@ -36,12 +36,14 @@ int8_t giveOk_P(PGM_P top, PGM_P promptText = MSG_LITTLE, PGM_P line2 = MSG_LITT
 int8_t giveOk_R(const char* top, const char* promptText = "", const char* line2 = "", const char* line3 = "");
 int8_t selectChoice(int howManyChoices, int initialUserInput, bool doNotClear = false);
 void setChoices_P(PGM_P label0=MSG_LITTLE,int value0=0,PGM_P label1=MSG_LITTLE,int value1=0,PGM_P label2=MSG_LITTLE,int value2=0,PGM_P label3=MSG_LITTLE,int value3=0,PGM_P label4=MSG_LITTLE,int value4=0,PGM_P label5=MSG_LITTLE,int value5=0,PGM_P label6=MSG_LITTLE,int value6=0,PGM_P label7=MSG_LITTLE,int value7=0,PGM_P label8=MSG_LITTLE,int value8=0,PGM_P label9=MSG_LITTLE,int value9=0);
+// RAM labels must have static storage; pointers are stored directly.
 void setChoices_R(const char *label0="",int value0=0,const char *label1="",int value1=0,const char *label2="",int value2=0,const char *label3="",int value3=0,const char *label4="",int value4=0,const char *label5="",int value5=0,const char *label6="",int value6=0,const char *label7="",int value7=0,const char *label8="",int value8=0,const char *label9="",int value9=0);
 void setChoicesHeader_P(PGM_P header=MSG_LITTLE);
 void setChoicesHeader_R(const char *header="");
 
 
 void setChoice_P(unsigned char index, PGM_P label, int value=0);
+// RAM labels must have static storage; pointers are stored directly.
 void setChoice_R(unsigned char index, const char *label, int value=0);
 
 long int inputNumber_P(PGM_P prompt, long int initialUserInput, int stepSize, long int min = 0, long int max = 100, PGM_P postFix = MSG_LITTLE, PGM_P optionalHeader = MSG_LITTLE);
