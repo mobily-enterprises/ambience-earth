d0a68b2ba2fb3bd14b86574b6b755fb217ad002d
Tony Mobily <tonymobily@gmail.com>
2026-01-02 19:16:50 +0800

    Compile for optiboot

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 -e main
+# Use the Optiboot/UNO target for more flash headroom (matches sim build).
+arduino-cli compile --fqbn arduino:avr:uno -e main
