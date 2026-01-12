13eb2f2eaaab0635c2dcd8cd3a75b2136c6dc458
Tony Mobily <tonymobily@gmail.com>
2026-01-12 18:33:37 +0800

    Reinstated partial redraw of menus

--- main/ui.cpp
+++ main/ui.cpp
@@ -910,22 +910,48 @@ int8_t selectChoice(int howManyChoices, int initialUserInput, bool doNotClear) {
     lcdPrintLabel(&choicesHeader);
   }
 
+  int lastSelectedIndex = -1;
+  int lastFirstVisibleIndex = -1;
+
   while (true) {
     if (displayChanged) {
-      for (int i = 0; i < maxVisibleChoices; i++) {
-        int choiceIndex = firstVisibleIndex + i;
-        lcd.setCursor(0, choicesStartY + i);
-        lcdPrintSpaces(DISPLAY_COLUMNS);
-        if (choiceIndex < howManyChoices) {
-          lcd.setCursor(0, choicesStartY + i);
-          if (choiceIndex == selectedIndex) {
-            lcd.write((uint8_t)2);  // Print cursor
-          } else {
-            lcd.print(' ');
+      if (firstVisibleIndex != lastFirstVisibleIndex) {
+        for (int i = 0; i < maxVisibleChoices; i++) {
+          int choiceIndex = firstVisibleIndex + i;
+          if (choiceIndex < howManyChoices) {
+            lcd.setCursor(0, choicesStartY + i);
+            lcdPrintSpaces(DISPLAY_COLUMNS);
+            lcd.setCursor(0, choicesStartY + i);
+            if (choiceIndex == selectedIndex) {
+              lcd.write((uint8_t)2);  // Print cursor
+            } else {
+              lcd.print(' ');
+            }
+
+            lcdPrintLabel(&choices[choiceIndex].label);
           }
-          lcdPrintLabel(&choices[choiceIndex].label);
+        }
+      } else if (lastSelectedIndex != -1 && lastSelectedIndex != selectedIndex) {
+        int prevRow = lastSelectedIndex - firstVisibleIndex;
+        int nextRow = selectedIndex - firstVisibleIndex;
+        if (prevRow >= 0 && prevRow < maxVisibleChoices) {
+          lcd.setCursor(0, choicesStartY + prevRow);
+          lcd.print(' ');
+        }
+        if (nextRow >= 0 && nextRow < maxVisibleChoices) {
+          lcd.setCursor(0, choicesStartY + nextRow);
+          lcd.write((uint8_t)2);
+        }
+      } else if (lastSelectedIndex == -1) {
+        int row = selectedIndex - firstVisibleIndex;
+        if (row >= 0 && row < maxVisibleChoices) {
+          lcd.setCursor(0, choicesStartY + row);
+          lcd.write((uint8_t)2);
         }
       }
+
+      lastSelectedIndex = selectedIndex;
+      lastFirstVisibleIndex = firstVisibleIndex;
       displayChanged = false;
     }
 
